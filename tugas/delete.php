c38ddadbf45d9ad590f554233f83694a54d4ad1c
anggarliliana <55338344+anggarliliana@users.noreply.github.com>
2019-11-07 08:48:19 +0700

    tugas
    tugasPrak8

---
+++ tugas/delete.php
@@ -0,0 +1,10 @@
+<?php 
+	include 'koneksi.php';
+	$nama= $_POST['nama'];
+	$query = "DELETE FROM mahasiswa where nama = '$nama'";
+	$sql=mysqli_query($conn, $query) or die(mysqli_error($conn));
+	if ($sql) {
+		echo "Data Berhasil Dihapus <br>";
+		echo "<a href='tugas.html'>kembali</a>";
+	}
+ ?>
